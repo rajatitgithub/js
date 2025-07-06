136a583e27376531eb26b0c1fff1dcfd5b91affe
Rajat <141062518+rajatitgithub@users.noreply.github.com>
2025-07-06 07:03:52 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using node js , not browser
+
+console.log(3 +3)
+console.log("Daddy ji")
+
+let name = "Rajat"
+let age = 23;
+let isLogged = true
+let state  = null
+let city;
+
+// number => 2 to power 53
+// bigint
+// String
+// boolean => true/false
+// null => standalone value
+// undefined => not defined value
+// symbol => unique
+
+// object 
+
+console.table([typeof name,typeof age,typeof isLogged, typeof state, typeof city]);
