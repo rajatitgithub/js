8fbcf7370fe8684cfc97a40c485a274ed69b470d
Rajat <141062518+rajatitgithub@users.noreply.github.com>
2025-07-11 17:13:22 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const name = "Rajat"
+const repoCount = 50
+
+// console.log(name + repoCount + " data");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String('Rajat-HC-Daddy');
+
+console.log(gameName.split('-'));
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf('H'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const anotherString = gameName.slice(-7,3);
+// console.log(anotherString);
+
+const newString1 = "    Rajat       ---yadav  ";
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url = "https://rajat.com/rajat%20yadav"
+// console.log(url.replace('%20' , '-'));
+// console.log(url.includes('sundar'));
