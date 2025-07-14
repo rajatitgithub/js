4dfb88375ac98dc8ea27db5f88eefe7d39cbeaf1
Rajat <141062518+rajatitgithub@users.noreply.github.com>
2025-07-14 18:12:12 +0000

    date

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -4,7 +4,6 @@ const score = 400
 console.log(score);
 console.log(typeof score);
 
-
 const balance = new Number(100);
 console.log(balance);
 
---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,17 @@
+// // Date
+
+let myDate = new Date();
+// console.log(myDate);                        //  2025-07-14T17:53:32.837Z
+// console.log(myDate.toString());             //  Mon Jul 14 2025 17:53:32 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString());         //  Mon Jul 14 2025
+// console.log(myDate.toISOString());          //  2025-07-14T17:56:18.369Z
+// console.log(myDate.toJSON());               //  2025-07-14T17:56:18.369Z
+// console.log(myDate.toLocaleDateString());   //  7/14/2025
+// console.log(myDate.toLocaleString());       //  7/14/2025, 5:56:18 PM
+// console.log(myDate.toLocaleTimeString());   //  5:56:18 PM
+
+// console.log(typeof myDate);
+
+// let myCreatDate = new Date(1998,6,8);
+let myCreatDate = new Date(2023, 0, 14)
+console.log(myCreatDate.toLocaleString());
